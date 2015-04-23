4896d2044ae5cb5e379655497a867b352ad981f1
TakayasuNasu <taka.beckham@gmail.com>
2015-04-23 20:19:14 +0900

    modified cron time

--- scripts/cron_bot.js
+++ scripts/cron_bot.js
@@ -1,7 +1,7 @@
 var cron = require('cron').CronJob;
 
 module.exports = function(robot) {
-  return new cron('10 */2 * * * 1-5', (function(_this) {
+  return new cron('10 10 10 * * 1-5', (function(_this) {
     return function() {
       return robot.send({
         room: "rails-project01"
