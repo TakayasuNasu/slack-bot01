7eda3b3388122ce7de1770470a469a046011e42d
TakayasuNasu <taka.beckham@gmail.com>
2015-04-23 19:40:49 +0900

    bug

--- scripts/cron_bot.js
+++ scripts/cron_bot.js
@@ -1,15 +1,15 @@
-var cronJob = require('cron').CronJob;
+// var cronJob = require('cron').CronJob;
 
-module.exports = function(robot) {
-  var cronOmikujiByHarada;
-  cronOmikujiByHarada = new cronJob('* */10 * * * 1,2,3,4,5', (function(_this) {
-    return function() {
-      var envelope;
-      envelope = {
-        room: "#random"
-      };
-      return robot.send(envelope, "原田さんおみくじ");
-    };
-  })(this));
-  return cronOmikujiByHarada.start();
-};
+// module.exports = function(robot) {
+//   var cronOmikujiByHarada;
+//   cronOmikujiByHarada = new cronJob('* */2 * * * 1,2,3,4,5', (function(_this) {
+//     return function() {
+//       var envelope;
+//       envelope = {
+//         room: "#random"
+//       };
+//       return robot.send(envelope, "原田さんおみくじ");
+//     };
+//   })(this));
+//   return cronOmikujiByHarada.start();
+// };
